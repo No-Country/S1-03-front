def4010f3d3b320cb8aa9c97d244612cfca1a258
Brian Delgado <braiandelgado4255@hotmail.com>
2022-03-31 16:05:00 -0300

    private route implemented

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Auth from '../Pages/Auth'
 import Home from '../Pages/Home'
 import NotFound from '../Pages/NotFound'
+import PrivateRoute from '../Utils/PrivateRoute'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes path="/">
-        <Route index element={<Home />} />
+        <Route element={<Home />} />
+        <Route index element={<PrivateRoute component={<Home />} />} />
+        <Route path="auth" element={<Auth />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
