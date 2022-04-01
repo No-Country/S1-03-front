02a083b10934704d3bed25186ddb9d7af25c5143
Brian Delgado <braiandelgado4255@hotmail.com>
2022-03-31 21:17:34 -0300

    providers & global styles added to app

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import Providers from '../context'
 import Auth from '../Pages/Auth'
 import Home from '../Pages/Home'
 import NotFound from '../Pages/NotFound'
+import GlobalStyles from '../Utils/globalStyles'
 import PrivateRoute from '../Utils/PrivateRoute'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes path="/">
-        <Route element={<Home />} />
-        <Route index element={<PrivateRoute component={<Home />} />} />
-        <Route path="auth" element={<Auth />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Providers>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes path="/">
+          <Route element={<Home />} />
+          <Route index element={<PrivateRoute component={<Home />} />} />
+          <Route path="auth" element={<Auth />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </Providers>
   )
 }
 
