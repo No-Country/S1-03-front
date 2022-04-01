a02ab47c32b44c6b9a3e516ecfe42bd88d08118d
Brian Delgado <braiandelgado4255@hotmail.com>
2022-03-31 22:06:15 -0300

    auth page base view

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Container from '../Components/Container'
+import ModalContainer from '../Components/ModalContainer'
 
 const Auth = () => {
-  return <div>Auth</div>
+  return (
+    <Container fluid fullHeight flex centered>
+      <ModalContainer>asd</ModalContainer>
+    </Container>
+  )
 }
 
 export default Auth
