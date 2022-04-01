7163110c06c90bef31203c5fc87f78dffb742a93
Brian Delgado <braiandelgado4255@hotmail.com>
2022-03-31 21:15:39 -0300

    theme provider added

---
+++ src/context/theme.jsx
@@ -0,0 +1,29 @@
+import propTypes from 'prop-types'
+import React, { useState, useEffect } from 'react'
+import { ThemeProvider } from 'styled-components'
+
+import { theme } from '../config/theme'
+
+const Theme = ({ children }) => {
+  const [darkmode, setDarkmode] = useState(false)
+  const [currentTheme, setCurrentTheme] = useState(theme.light)
+
+  const changeTheme = () => {
+    setDarkmode(!darkmode)
+  }
+  useEffect(() => {
+    darkmode ? setCurrentTheme(theme.dark) : setCurrentTheme(theme.light)
+  }, [darkmode])
+
+  return (
+    <ThemeProvider theme={{ ...currentTheme, changeTheme, fonts: theme.fonts }}>
+      {children}
+    </ThemeProvider>
+  )
+}
+
+Theme.propTypes = {
+  children: propTypes.node
+}
+
+export default Theme
