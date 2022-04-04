4c73f26d79f363820ee00d19de96c432e7a5fc40
Brian Delgado <braiandelgado4255@hotmail.com>
2022-04-04 13:40:44 -0300

    Home cointains the chat

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Chat from '../Components/Chat/'
+import Container from '../Components/Container'
 
 const Home = () => {
-  return <>Home</>
+  return (
+    <Container w="70%" h="100%">
+      <Chat />
+    </Container>
+  )
 }
 
 export default Home
