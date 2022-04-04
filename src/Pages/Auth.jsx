e43e529de51ff0e338366ca95f19c46add8960b1
Martinmeza0 <martinmezacorreo@gmail.com>
2022-04-04 20:10:16 -0300

    form component added

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Container from '../Components/Container'
 import ModalContainer from '../Components/ModalContainer'
-
+import Form from '../Components/Form'
 const Auth = () => {
   return (
-    <Container fluid fullHeight flex centered>
-      <ModalContainer>asd</ModalContainer>
+    <Container fullHeight flex centered>
+      <ModalContainer>
+        <Form></Form>
+      </ModalContainer>
     </Container>
   )
 }
