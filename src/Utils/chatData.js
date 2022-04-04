21d7c06920c68008db1c92c9db770ca3691a4005
Brian Delgado <braiandelgado4255@hotmail.com>
2022-04-04 13:45:12 -0300

    chat data for development

---
+++ src/Utils/chatData.js
@@ -0,0 +1,16 @@
+export const messages = [
+  {
+    id: 1,
+    date: new Date().toLocaleDateString(),
+    sender: 1,
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos molestias fuga iure expedita illum, similique optio totam voluptatem doloribus necessitatibus ullam eligendi dolorum perspiciatis mollitia. Reprehenderit voluptas dolor explicabo odio!'
+  },
+  {
+    id: 2,
+    date: new Date().toLocaleDateString(),
+    sender: 2,
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos molestias fuga iure expedita illum, similique optio totam voluptatem doloribus necessitatibus ullam eligendi dolorum perspiciatis mollitia. Reprehenderit voluptas dolor explicabo odio!'
+  }
+]
