f99c7b3f7ab581740d151523140ce6ce2f2f894c
Martinmeza0 <martinmezacorreo@gmail.com>
2022-04-04 20:10:06 -0300

    update: added firstname and lastname input

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 
 const Form = () => {
+  const { register, handleSubmit } = useForm()
   return (
-    <div>Form</div>
+    <>
+      <form onSubmit={handleSubmit((data) => {
+        console.log(data)
+      })}>
+        <input {...register('firstname')} placeholder="First Name"/>
+        <input {...register('lastname')} placeholder="Last Name"/>
+        <input type="submit"/>
+      </form>
+    </>
   )
 }
 
