532b7cdaf9a94d57b3918d4aba724d048a67b462
Brian Delgado <braiandelgado4255@hotmail.com>
2022-04-05 16:10:59 -0300

    general props added

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -3,13 +3,26 @@ import styled, { css } from 'styled-components'
 
 // prettier-ignore
 const Container = styled.div`
+  ${props => props.bgc && css`background-color: ${props => props.bgc};`}
+  ${props => props.overflow && css`overflow: ${props.overflow};`}
+  /* Sizing */
   width: ${props => props.w || '100%'};
-  margin: ${props => props.margin || '0 auto'};
-  height: ${props => props.h || '100%'};
+  ${props => props.h && css`height: ${props.h};`}
   max-width: ${props => (props.fluid ? '100%' : '90%')};
-  ${props => props.fullHeight && css`min-height: 100vh;`}
+  ${props => props.mh && css`max-height: ${props.mh};`}
+  margin: ${props => props.margin || '0 auto'};
   ${props => props.p && css`padding: ${props.p};`}
+  /* Position options */
+  ${props => props.pos && css`position: ${props => props.pos};`}
+  ${props => props.pos && props.top && css`top: ${props.top};`}
+  ${props => props.pos && props.bottom && css`bottom: ${props.bottom};`}
+  ${props => props.pos && props.left && css`left: ${props.left};`}
+  ${props => props.pos && props.right && css`right: ${props.right};`}
+  /* Flex options*/
   ${props => props.flex && css`display: flex;`}
+  ${props => props.flex && props.jc && css`justify-content: ${props.jc};`}
+  ${props => props.flex && props.ai && css`align-items: ${props.ai};`}
+  ${props => props.flex && props.direction && css`flex-direction: ${props.direction};`}
   ${props => props.flex && props.centerMain && css`justify-content: center;`}
   ${props => props.flex && props.centerCross && css`align-items: center;`}
   ${props => props.flex && props.gap && css`gap: ${props.gap};`}
@@ -29,6 +42,8 @@ Container.propTypes = {
   h: propTypes.string,
   m: propTypes.string,
   gap: propTypes.string,
-  p: propTypes.string
+  p: propTypes.string,
+  bgc: propTypes.string,
+  pos: propTypes.string
 }
 export default Container
