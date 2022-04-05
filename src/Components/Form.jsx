3f7027904770bb2d37ea1a0909e23c948822eaa2
martinmeza0 <martinmezacorreo@gmail.com>
2022-04-05 17:43:34 -0300

    adding styles to form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import styled from 'styled-components'
 import { palette } from '../config/theme'
+import { Icon } from '@iconify/react'
 
 const Form = () => {
   const { register, handleSubmit, formState: { errors } } = useForm()
@@ -35,32 +36,39 @@ const Form = () => {
             />
 
           {errors.email && <Errors> {errors.email.message}</Errors>}
-          <FormInput placeholder="Email" autoComplete="off"
-            {...register('email',
-              {
-                required: 'Enter the email',
-                minLength: {
-                  value: 12,
-                  message: 'The email is too short'
-                },
-                pattern: {
-                  value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                  message: 'Please enter a valid email'
-                }
-              })}
-            />
+
+          <InputContainer>
+            <Icon icon="mdi:account" />
+            <FormInput placeholder="Email" autoComplete="off"
+              {...register('email',
+                {
+                  required: 'Enter the email',
+                  minLength: {
+                    value: 12,
+                    message: 'The email is too short'
+                  },
+                  pattern: {
+                    value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    message: 'Please enter a valid email'
+                  }
+                })}
+              />
+          </InputContainer>
 
           {errors.password && <Errors> {errors.password.message}</Errors>}
-          <FormInput placeholder="Password" type='password' autoComplete="off"
-            {...register('password',
-              {
-                required: 'password is required',
-                minLength: {
-                  value: 10,
-                  message: 'the pasword must be at least 10 characters'
-                }
-              })}
-            />
+          <InputContainer>
+            <Icon icon="ri:lock-password-fill" />
+            <FormInput placeholder="Password" type='password' autoComplete="off"
+              {...register('password',
+                {
+                  required: 'password is required',
+                  minLength: {
+                    value: 10,
+                    message: 'the pasword must be at least 10 characters'
+                  }
+                })}
+              />
+          </InputContainer>
 
           <FormButton type="submit"/>
         </form>
@@ -68,16 +76,24 @@ const Form = () => {
   )
 }
 
+const InputContainer = styled.div`
+  display: flex;
+  & > div {
+    background-color: ${palette.blue};
+  }
+`
+
 const FormInput = styled.input`
   width: 100%;
   margin-bottom: 10px;
   display: block;
-  padding: 6px;
+  padding: 10px;
 `
 const FormButton = styled.input`
   background-color: ${palette.blue};
   color: ${palette.white};
   padding: 10px 25px;
+  width: 20%;
   border: none;
 `
 
