3c172e8ddea26383cd4b6437ff4751f8eaf3ac92
Brian Delgado <braiandelgado4255@hotmail.com>
2022-04-05 16:11:37 -0300

    Container Added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import Chat from '../Components/Chat/'
-import Container from '../Components/Container'
 
 const Home = () => {
-  return (
-    <Container w="70%" h="100%">
-      <Chat />
-    </Container>
-  )
+  return <Chat />
 }
 
 export default Home
