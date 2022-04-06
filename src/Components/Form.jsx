702162ae45d293d132e413df7edde3a2c8f93da2
martinmeza0 <martinmezacorreo@gmail.com>
2022-04-06 17:17:50 -0300

    reform the form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -12,17 +12,7 @@ const Form = () => {
           console.log(data)
         })}>
 
-          {errors.firstName && <Errors> {errors.firstName.message}</Errors>}
-          <FormInput placeholder="First Name" autoComplete="on"
-            {...register('firstName',
-              {
-                required: 'Name is required',
-                maxLength: {
-                  value: 20,
-                  message: 'The name is too long'
-                }
-              })}
-            />
+
           {errors.lastName && <Errors> {errors.lastName.message}</Errors>}
           <FormInput placeholder="Last Name" autoComplete="on"
             {...register('lastName',
@@ -35,7 +25,6 @@ const Form = () => {
               })}
             />
 
-          {errors.email && <Errors> {errors.email.message}</Errors>}
 
           <InputContainer>
             <Icon icon="mdi:account" />
@@ -55,19 +44,8 @@ const Form = () => {
               />
           </InputContainer>
 
-          {errors.password && <Errors> {errors.password.message}</Errors>}
           <InputContainer>
             <Icon icon="charm:eye" />
-            <FormInput placeholder="Password" type='password' autoComplete="off"
-              {...register('password',
-                {
-                  required: 'password is required',
-                  minLength: {
-                    value: 10,
-                    message: 'the pasword must be at least 10 characters'
-                  }
-                })}
-              />
           </InputContainer>
 
           <FormButton type="submit" value="Register"/>
