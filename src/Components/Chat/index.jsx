e24ff8b6e27933d8549598acdc6c1471e17d9e9b
Brian Delgado <braiandelgado4255@hotmail.com>
2022-04-06 12:46:34 -0300

    Chat cointaner moved to layout

--- src/Components/Chat/index.jsx
+++ src/Components/Chat/index.jsx
@@ -4,26 +4,17 @@ import { messages } from '../../Utils/chatData'
 import ChatInput from './ChatInput'
 import ChatContainer from './ChatContainer'
 import TopBar from './TopBar'
-import Container from '../Container'
 
 const Chat = () => {
   const user = 2
   const [msgList, setMsgList] = useState(messages)
 
   return (
-    <Container
-      fluid
-      flex
-      direction="column"
-      ai="space-between"
-      bgc={'#ddf'}
-      w="70%"
-      h="100vh"
-      margin="0 0 0 auto">
+    <>
       <TopBar />
       <ChatContainer msg={msgList} user={user} />
-      <ChatInput func={setMsgList} list={msgList}/>
-    </Container>
+      <ChatInput func={setMsgList} list={msgList} />
+    </>
   )
 }
 
