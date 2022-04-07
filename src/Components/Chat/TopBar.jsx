7eac45e38e7bdb40bb12bef8305238393e492c59
Brian Delgado <braiandelgado4255@hotmail.com>
2022-04-07 20:50:52 -0300

    spacing for burger menu

--- src/Components/Chat/TopBar.jsx
+++ src/Components/Chat/TopBar.jsx
@@ -9,7 +9,7 @@ const TopBar = () => {
     'https://64.media.tumblr.com/f5ec39d6fa69304a28f5755279ea4c76/178d467c126f1716-72/s500x750/68130d0b13e9837264c583c3eedc04ac10c6fb0d.jpg'
   return (
     <Container fluid bgc={primary}>
-      <Container flex h="70px" centerCross gap="1rem">
+      <Container flex h="70px" centerCross gap="1rem" md_p="0 0 0 3rem">
         <UserImg src={img} rounded /> Firstname Lastname
       </Container>
     </Container>
