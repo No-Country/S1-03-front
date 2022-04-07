9a8e03f5101c9f64e97ddbea79ab82b7758b6e7e
Martinmeza0 <martinmezacorreo@gmail.com>
2022-04-07 10:17:29 -0300

    created a new style archive for each forms

---
+++ src/Components/Forms/FormStyles.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+import { palette } from '../../config/theme'
+
+export const FormContainer = styled.form`
+  width: 100%;
+`
+
+export const InputContainer = styled.div`
+  display: flex;
+  vertical-align: middle;
+  & > svg {
+    background-color: ${palette.blue};
+    font-size: 2.5rem;
+    padding: 6px;
+    color: ${palette.white}
+  }
+`
+
+export const FormInput = styled.input`
+  width: 100%;
+  margin-bottom: 10px;
+  display: block;
+  padding: 10px;
+  outline: none;
+  border: none;
+  background: ${props => props.theme.inputBg};
+  transition: box-shadow .2s;
+  &::placeholder {
+    color: ${props => props.theme.inputPlaceholder};
+  }
+  &:focus {
+    box-shadow: inset 0px -1px 1px 0px ${palette.blue};
+  }
+`
+export const FormButton = styled.input`
+  background-color: ${palette.blue};
+  color: ${palette.white};
+  padding: 10px 25px;
+  text-align: center;
+  border: none;
+  float: right;
+  transition: background-color .2s;
+  cursor: pointer;
+  &:hover {
+    background-color: ${palette.green};
+  }
+`
+
+export const Errors = styled.span`
+  display: block;
+  color: ${palette.red};
+  font-size: 0.7rem;
+
+`
