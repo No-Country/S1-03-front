0975c56179c5c257f71eebae03572f34c87f4657
Brian Delgado <braiandelgado4255@hotmail.com>
2022-04-20 08:18:44 -0300

    initial sockets set

--- src/Components/Chat/index.jsx
+++ src/Components/Chat/index.jsx
@@ -4,16 +4,30 @@ import { messages } from '../../Utils/chatData'
 import ChatInput from './ChatInput'
 import ChatContainer from './ChatContainer'
 import TopBar from './TopBar'
+import Socket from '../../Services/socketio'
+import Auth from '../../Services/auth'
 
 const Chat = () => {
   const user = 2
   const [msgList, setMsgList] = useState(messages)
+
+  const onClick = () => {
+    Socket.connectQueue()
+  }
+  const sendMessage = () => {
+    Socket.sendMessage('yo', 'hello xdxdxd')
+  }
 
   return (
     <>
       <TopBar />
       <ChatContainer msg={msgList} user={user} />
       <ChatInput func={setMsgList} list={msgList} />
+      <div>
+        <button onClick={onClick}>conect</button>
+        <button onClick={Auth.login}>Login</button>
+        <button onClick={sendMessage}>msg</button>
+      </div>
     </>
   )
 }
--- src/Services/auth.js
+++ src/Services/auth.js
@@ -1,11 +1,26 @@
 class Auth {
   constructor() {
-    this.auth = false
+    this.auth = true
     this.user = {}
     this.token = ''
   }
 
   async login() {
+    const res = await fetch(
+      'https://nocountry-chat.herokuapp.com/api/auth/signin',
+      {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: 'braian123',
+          password: '12345678'
+        })
+      }
+    )
+    console.log(await res.json())
+
     this.auth = true
   }
 
