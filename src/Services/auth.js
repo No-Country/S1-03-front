32b0af713f35205546a6cd28a93b10693466db4e
Brian Delgado <braiandelgado4255@hotmail.com>
2022-04-21 23:52:25 -0300

    provisional commit

--- src/Services/auth.js
+++ src/Services/auth.js
@@ -1,19 +1,35 @@
+import axios from 'axios'
+
 class Auth {
   constructor() {
-    this.auth = true
+    this.auth = false
     this.user = {}
     this.token = ''
+    this.url = 'https://nocountry-chat.herokuapp.com'
   }
 
-  async login() {
-    this.auth = true
+  async login(user) {
+    const res = await axios.post(`${this.url}/api/auth/signin`, {
+      username: user.username,
+      password: user.password
+    })
+
+    console.log(res.data)
   }
 
-  async register() {
-    this.auth = true
+  async register(user) {
+    const res = axios.post(`${this.url}/api/auth/signup`, {
+      name: user.name,
+      lastname: user.name,
+      username: user.username,
+      email: user.email,
+      password: user.password
+    })
+    console.log(res)
   }
 
-  saveToken() {}
+  saveToken(token) {}
+
   logout() {
     this.auth = false
   }
